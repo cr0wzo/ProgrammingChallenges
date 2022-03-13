01a658f1cc4f23f38794453d0e408748752987e0
cr0wzo <cr0wzothesecond@gmail.com>
2022-03-13 10:54:45 -0700

    Messed And Fixed 001 and Added 000

---
+++ 000/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyProject
+{
+    /*
+    Project 000: Name Generator
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FirstNameFile = "FirstNames.txt";
+            const string LastNameFile = "LastNames.txt";
+
+            Random random = new Random();
+            string[] firstNames = File.ReadAllLines(FirstNameFile);
+            string[] lastNames = File.ReadAllLines(LastNameFile);
+
+            int randFirstName = File.ReadLines(FirstNameFile).Count();
+            int randLastName = File.ReadLines(LastNameFile).Count();
+
+            Console.WriteLine("{0} {1}", firstNames[random.Next(0, randFirstName)], lastNames[random.Next(0, randLastName)]);
+        }
+    }
+}
