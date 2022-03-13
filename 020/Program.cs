927ae82d2584aa14b536f8a0ec12d44c66b387ec
cr0wzo <cr0wzothesecond@gmail.com>
2022-03-12 22:51:48 -0700

    Added 020

---
+++ 020/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyProject
+{
+    /*
+    Project 040: BMI Calculator
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Weight In Kilograms?");
+                int weight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Height Meters?");
+                float height = (float) Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Your BMI is {0}", weight / (height * height));
+            } catch(FormatException)
+            {
+                Console.WriteLine("Please input a correct amount");
+            } 
+        }
+    }
+}
