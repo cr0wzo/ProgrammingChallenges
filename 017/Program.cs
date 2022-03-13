b6b4b2c14e1f6c687d259689b690aa01d51d3b2e
cr0wzo <cr0wzothesecond@gmail.com>
2022-03-12 20:37:48 -0700

    Added 017

---
+++ 017/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyProject
+{
+    /*
+    Project 017: Count The Words In A String
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a sentence");
+            char[] sentence = Console.ReadLine().ToCharArray();
+            
+            int count = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                try
+                {
+                    if (sentence[i] != ' ' && sentence[i + 1] == ' ')
+                    {
+                        count++;
+                    }
+                } catch
+                {
+                    count++;
+                }
+                
+                Console.WriteLine(sentence[i]);
+            }
+
+            Console.WriteLine("There are {0} words in that sentence", count);
+        }
+    }
+}
