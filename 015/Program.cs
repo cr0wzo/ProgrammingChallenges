1b0a66d7d0a2e2038520a11aec9c4e58c19b694d
cr0wzo <cr0wzothesecond@gmail.com>
2022-03-12 17:38:29 -0700

    Added 015

---
+++ 015/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyProject
+{
+    /*
+    Project 015: Reverse a string
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a sentence");
+            string sentence = Console.ReadLine();
+            char[] splitSentence = sentence.ToCharArray();
+
+            for (int i = splitSentence.Length - 1; i >= 0 ; i--)
+            {
+                Console.Write(splitSentence[i]);
+            } Console.WriteLine();
+        }
+    }
+}
