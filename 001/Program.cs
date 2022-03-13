95204132f1abc76168b57956b2d33d05b8b8d918
cr0wzo <cr0wzothesecond@gmail.com>
2022-03-12 22:08:40 -0700

    Added 001

---
+++ 001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyProject
+{
+    /*
+    Project 001: Heads Or Tails
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            if (random.Next(0,2) == 0)
+            {
+                Console.WriteLine("Heads!");
+            } else 
+            {
+                Console.WriteLine("Tails!");
+            }
+        }
+    }
+}
